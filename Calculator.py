17a6e613d10fcd4cdf3a4d47dcc7955a2cb55a61
Diogo de Souza <di_smgomes@hotmail.com>
2022-01-14 14:33:15 -0300

    Calculator aplication developed using while, function and if/else

---
+++ Calculator.py
@@ -0,0 +1,58 @@
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+def add(n1, n2):
+    return n1 + n2
+
+def substract (n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": substract,
+    "*": multiply,
+    "/": divide
+}
+
+
+def calculator():
+    print(logo)
+    num1 = float(input("What's the first number?: "))
+    for op in operations:
+        print(op)
+    end_calculator = True
+
+    while end_calculator:
+
+        symbol = input("Pick an symbol operator: ")
+        num2 = float(input("What's the second number?: "))
+        result = operations[symbol](num1,num2)
+        print("{} {} {} = {}".format(num1, symbol, num2, result))
+
+        progression = input("Do you want calculating {}, type 'y' or type 'n' to restart calculator.\n".format(result))
+        if progression.lower() == "y":
+            num1 = result
+        else:
+            end_calculator = False
+            calculator()
+
+calculator()
